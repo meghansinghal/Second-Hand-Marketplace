44156542cf270b1367c80fc262a259adbdc31c8f
Meghan Singhal <meghansinghal@gmail.com>
2026-03-16 15:47:50 +0530

    Add MVC project structure and source files

---
+++ src/model/User.java
@@ -0,0 +1,11 @@
+package model;
+
+public class User {
+
+    protected int userId;
+    protected String name;
+    protected String email;
+    protected String password;
+    protected String phone;
+
+}
